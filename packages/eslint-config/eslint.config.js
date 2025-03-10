3d8ef5812fff1edf11b329cb660b380792c0ffe2
François Lachèse <francois.lachese@gmail.com>
2025-03-10 18:36:09 +0100

    build: improve eslint config

--- packages/eslint-config/eslint.config.js
+++ packages/eslint-config/eslint.config.js
@@ -2,13 +2,15 @@ import pluginJs from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import eslintPluginUnicorn from "eslint-plugin-unicorn";
-
 import stylisticJs from "@stylistic/eslint-plugin-js";
+import stylisticTs from "@stylistic/eslint-plugin-ts";
+
+// TODO: Clean and organize this file
 export default [
-  { files: ["**/*.{ts}"] },
+  { files: ["**/*.{ts,js}"] },
   { languageOptions: { globals: globals.node } },
-  { plugins: { "@stylistic/js": stylisticJs } },
-  { rules: { semi: "error", "@stylistic/js/quotes": ["error", "double"] } },
+  { plugins: { "@stylistic/js": stylisticJs, "@stylistic/ts": stylisticTs } },
+  { rules: { "@stylistic/ts/semi": ["error", "always"], "@stylistic/js/semi": ["error", "always"], "@stylistic/js/quotes": ["error", "double"] } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginUnicorn.configs.recommended,
