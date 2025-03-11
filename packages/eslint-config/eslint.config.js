f8123f3f4197be8ea6ac43ca395d7721869000b6
François Lachèse <francois.lachese@gmail.com>
2025-03-11 23:53:31 +0100

    build: add prettier

--- packages/eslint-config/eslint.config.js
+++ packages/eslint-config/eslint.config.js
@@ -4,14 +4,22 @@ import tseslint from "typescript-eslint";
 import eslintPluginUnicorn from "eslint-plugin-unicorn";
 import stylisticJs from "@stylistic/eslint-plugin-js";
 import stylisticTs from "@stylistic/eslint-plugin-ts";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 // TODO: Clean and organize this file
 export default [
   { files: ["**/*.{ts,js}"] },
   { languageOptions: { globals: globals.node } },
   { plugins: { "@stylistic/js": stylisticJs, "@stylistic/ts": stylisticTs } },
-  { rules: { "@stylistic/ts/semi": ["error", "always"], "@stylistic/js/semi": ["error", "always"], "@stylistic/js/quotes": ["error", "double"] } },
+  {
+    rules: {
+      "@stylistic/ts/semi": ["error", "always"],
+      "@stylistic/js/semi": ["error", "always"],
+      "@stylistic/js/quotes": ["error", "double"],
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginUnicorn.configs.recommended,
+  eslintPluginPrettierRecommended,
 ];
