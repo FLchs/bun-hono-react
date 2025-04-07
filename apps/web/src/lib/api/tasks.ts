c725e9219c722376b154cdab1a7bc13cb4065d0d
François Lachèse <francois.lachese@gmail.com>
2025-04-07 17:23:02 +0200

    feat(web): add get task route functions

--- apps/web/src/lib/api/tasks.ts
+++ apps/web/src/lib/api/tasks.ts
@@ -1,13 +1,29 @@
 import { InferRequestType, InferResponseType } from "hono";
 import { client } from "../client";
+import { queryOptions } from "@tanstack/react-query";
 
-export const getTasks = async () => {
+const getTasks = async () => {
   const response = await client.api.tasks.$get();
   return await response.json();
 };
 
+export const getTasksQueryOption = queryOptions({
+  queryKey: ["tasks"],
+  queryFn: getTasks,
+});
+
+const getTask = async (id: string) => {
+  const response = await client.api.tasks[":id{[0-9]+}"].$get({ param: { id } });
+  return await response.json();
+};
+
+export const getTaskQueryOption = (id: string) => queryOptions({
+  queryKey: ["task", id],
+  queryFn: () => getTask(id),
+});
+
 export type TasksResponse = InferResponseType<typeof client.api.tasks.$get>;
-export type Tasks = TasksResponse["tasks"];
+export type Tasks = TasksResponse;
 export type Task = Tasks[number];
 
 export const createTask = async (
