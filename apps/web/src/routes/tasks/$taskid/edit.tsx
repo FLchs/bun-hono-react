b5f5d4474ac33a192731623925f37dfac05af7ea
François Lachèse <francois.lachese@gmail.com>
2025-04-07 18:52:51 +0200

    feat(web): add task edition and clean up code

--- apps/web/src/routes/tasks/$taskid/edit.tsx
+++ apps/web/src/routes/tasks/$taskid/edit.tsx
@@ -1,7 +1,8 @@
+import { TaskForm } from "@/components/tasks/form";
 import { Button } from "@/components/ui/button";
-import { getTaskQueryOption } from "@/lib/api/tasks";
-import { useQuery } from "@tanstack/react-query";
-import { createFileRoute, Link } from "@tanstack/react-router";
+import { getTaskQueryOption, updateTask } from "@/lib/api/tasks";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { createFileRoute, Link, useRouter } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/tasks/$taskid/edit")({
   component: RouteComponent,
@@ -11,25 +12,36 @@ export const Route = createFileRoute("/tasks/$taskid/edit")({
 });
 
 function RouteComponent() {
+  const queryClient = useQueryClient();
   const { taskid } = Route.useParams();
   const { data: task, isPending } = useQuery(getTaskQueryOption(taskid));
+  const router = useRouter();
+  const { mutate } = useMutation({
+    mutationFn: updateTask(taskid),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["task", taskid] });
+      router.history.back();
+    },
+  });
 
   if (isPending) return <p>Loading...</p>;
   if (task == undefined) return <p>404</p>;
 
   return (
-    <div style={{ textAlign: "left" }}>
-      <Link to="/tasks" className="mb-2">
-        <Button variant="secondary">⬅ Back</Button>
-      </Link>
-      <p>
-        <strong>Title: </strong>
-        {task.title}
-      </p>
-      <p>
-        <strong> Description: </strong>
-        {task.description}
-      </p>
-    </div>
+    <>
+      <div style={{ textAlign: "left" }}>
+        <Link to="/tasks" className="mb-2">
+          <Button variant="secondary">⬅ Back</Button>
+        </Link>
+        <TaskForm
+          handleSubmit={(data) => mutate({ json: data, param: { id: taskid } })}
+          defaultValues={{
+            title: task.title,
+            description: task.description,
+            status: task.status,
+          }}
+        />
+      </div>
+    </>
   );
 }
--- apps/web/src/routes/tasks/index.tsx
+++ apps/web/src/routes/tasks/index.tsx
@@ -1,14 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { AnyFieldApi, useForm } from "@tanstack/react-form";
-import { taskInsertSchema, taskStatus, TaskStatus } from "@cm3k/api/schema";
-import { z } from "zod";
 import { createTask, getTasksQueryOption } from "@api/tasks";
-import { TextInput } from "@/components/form/input/text";
-import { SelectInput } from "@/components/form/input/select";
-import { TextArea } from "@/components/form/input/textarea";
 import { TaskItem } from "@/components/tasks/task";
-import { Button } from "@/components/ui/button";
+import { TaskForm } from "@/components/tasks/form";
 
 export const Route = createFileRoute("/tasks/")({
   component: TasksList,
@@ -29,109 +23,16 @@ function TasksList() {
     },
   });
 
-  const form = useForm({
-    defaultValues: {
-      status: taskStatus[0],
-    } as z.infer<typeof taskInsertSchema>,
-    validators: {
-      onChange: taskInsertSchema,
-    },
-    onSubmit({ value, formApi }) {
-      mutate({ json: value });
-      formApi.reset();
-    },
-  });
-
   return (
     <>
       <h1 className="text-2xl">Tasks</h1>
-      <form
-        className="flex flex-col gap-4 my-4 w-96"
-        onSubmit={(event) => {
-          event.preventDefault();
-          form.handleSubmit();
-        }}
-      >
-        <form.Field
-          name="title"
-          children={(field) => (
-            <>
-              <TextInput
-                title="Title:"
-                id={field.name}
-                name={field.name}
-                value={field.state.value ?? ""}
-                onBlur={field.handleBlur}
-                onChange={(event) => field.handleChange(event.target.value)}
-              />
-              <FieldInfo field={field} />
-            </>
-          )}
-        />
-        <form.Field
-          name="status"
-          children={(field) => (
-            <>
-              <SelectInput
-                title="Status:"
-                id={field.name}
-                name={field.name}
-                value={field.state.value as TaskStatus}
-                onBlur={field.handleBlur}
-                onChange={(event) =>
-                  field.handleChange(event.target.value as TaskStatus)
-                }
-              >
-                {taskStatus.map((status, index) => (
-                  <option key={index} value={status}>
-                    {status.replace("_", " ")}
-                  </option>
-                ))}
-                <option>wrong</option>
-              </SelectInput>
-              <FieldInfo field={field} />
-            </>
-          )}
-        />
-        <form.Field
-          name="description"
-          children={(field) => (
-            <>
-              <TextArea
-                title="Description:"
-                id={field.name}
-                name={field.name}
-                value={field.state.value ?? ""}
-                onBlur={field.handleBlur}
-                onChange={(event) => field.handleChange(event.target.value)}
-              />
-              <FieldInfo field={field} />
-            </>
-          )}
-        />
-        <Button type="submit">
-          {form.state.isSubmitting ? "..." : "Submit"}
-        </Button>
-      </form>
 
+      <TaskForm handleSubmit={(data) => mutate({ json: data })} />
       <p>{error?.message}</p>
       <pre>{error?.issues}</pre>
       {isPending
         ? "Loading..."
         : data?.map((task) => <TaskItem key={task.id} task={task} />)}
-    </>
-  );
-}
-
-function FieldInfo({ field }: { field: AnyFieldApi }) {
-  return (
-    <>
-      {field.state.meta.isTouched && field.state.meta.errors.length > 0 ? (
-        <em>
-          {field.state.meta.errors.map((error) => error.message).join(",")}
-        </em>
-      ) : undefined}
-      {field.state.meta.isValidating ? "Validating..." : undefined}
     </>
   );
 }
